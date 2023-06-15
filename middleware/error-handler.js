7dbfaa0a8fe220ec5cea99f94666dd961838a879
LLwheaton <lwheaton89@gmail.com>
2023-06-15 22:11:14 +1000

    Add http-status-codes

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,14 @@
+import { StatusCodes } from 'http-status-codes';
+
 // Handle errors that happen in the existing routes
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err);
-    res.status(500).json({msg: err});
+    const defaultError = {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: 'Something went wrong, try again later',
+    }
+    
+    res.status(defaultError.statusCode).json({msg: err});
 }
 
 export default errorHandlerMiddleware
