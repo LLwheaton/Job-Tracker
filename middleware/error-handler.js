fc350c91cf98f7cdb6ec8b989d44b6dd216f0cac
LLwheaton <lwheaton89@gmail.com>
2023-06-15 20:36:51 +1000

    Pass error to middleware handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 // Handle errors that happen in the existing routes
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err);
-    res.status(500).json({msg:'There was an error'});
+    res.status(500).json({msg: err});
 }
 
 export default errorHandlerMiddleware
--- models/User.js
+++ models/User.js
@@ -11,6 +11,7 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String, 
+        required: [true, 'Please provide email'],
         validate: {
             validator: validator.isEmail,
             message: 'Please provide a valid email'
